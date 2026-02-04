1f0f9cec8e0e457129e5bcb2e519d586cc96bbf0
Мартын Анастасия < @LAPTOP-A2M7F0P3>
2026-02-04 15:03:27 +0300

    True

--- tests/kmeans_unchar16d_tests.cpp
+++ tests/kmeans_unchar16d_tests.cpp
@@ -179,23 +179,33 @@ namespace {
 
 
     static std::vector<KMeansUniversalParams> synthetic_data_kmeans_student2_u8c32 = {
-        { ClustersCount{ 5 }, PointsCount{ 1000 }, Attempts{ 3 },
-          Perf{ false }, IsKMeansPlusPlus{ false } },
-        { ClustersCount{ 10 }, PointsCount{ 5000 }, Attempts{ 3 },
-          Perf{ false }, IsKMeansPlusPlus{ false } },
-        { ClustersCount{ 3 }, PointsCount{ 500 }, Attempts{ 5 },
+        { ClustersCount{ 5 }, PointsCount{ 1000 }, Attempts{ 10 },
+          Perf{ false }, IsKMeansPlusPlus{ true } },
+        { ClustersCount{ 10 }, PointsCount{ 5000 }, Attempts{ 10 },
+          Perf{ false }, IsKMeansPlusPlus{ true } },
+        { ClustersCount{ 3 }, PointsCount{ 500 }, Attempts{ 10 },
           Perf{ false }, IsKMeansPlusPlus{ true } },
     };
 
 
     static std::vector<KMeansUniversalParams> synthetic_data_kmeans_student2_u8c32_perf = {
-        { ClustersCount{ 5 }, PointsCount{ 20000 }, Attempts{ 5 },
-          Perf{ true }, IsKMeansPlusPlus{ false } },
-        { ClustersCount{ 10 }, PointsCount{ 50000 }, Attempts{ 3 },
-          Perf{ true }, IsKMeansPlusPlus{ false } },
-        { ClustersCount{ 15 }, PointsCount{ 100000 }, Attempts{ 2 },
-          Perf{ true }, IsKMeansPlusPlus{ false } },
+        { ClustersCount{ 5 }, PointsCount{ 20000 }, Attempts{ 10 },
+          Perf{ true }, IsKMeansPlusPlus{ true } },
+        { ClustersCount{ 10 }, PointsCount{ 50000 }, Attempts{ 10 },
+          Perf{ true }, IsKMeansPlusPlus{ true } },
+        { ClustersCount{ 15 }, PointsCount{ 100000 }, Attempts{ 10 },
+          Perf{ true }, IsKMeansPlusPlus{ true } },
     };
+
+    static std::vector<KMeansUniversalParams> synthetic_data_kmeans_student2_u8c32_perf = {
+    { ClustersCount{ 5 }, PointsCount{ 20000 }, Attempts{ 10 },
+      Perf{ true }, IsKMeansPlusPlus{ true } },
+    { ClustersCount{ 10 }, PointsCount{ 50000 }, Attempts{ 10 },
+      Perf{ true }, IsKMeansPlusPlus{ true } },
+    { ClustersCount{ 15 }, PointsCount{ 100000 }, Attempts{ 10 },
+      Perf{ true }, IsKMeansPlusPlus{ true } },
+    };
+
 
     INSTANTIATE_TEST_SUITE_P(Accuracy, SyntheticTestKMeansStudent2_u8c16,
         testing::ValuesIn(synthetic_data_kmeans_student2_u8c32));
